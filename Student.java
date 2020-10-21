3c64ab610cb7cfdc09e5deec9321ec27e9f27987
Aishwarya <amuthyala511@gmail.com>
2020-10-21 18:30:30 +0530

    added new lines to Student java file

--- Student.java
+++ Student.java
@@ -4,8 +4,10 @@ public class Student
 	public static void main(String[] args)
 	{
 		System.out.println("Enter roll number: ");
+		System.out.println("Enter age: ");
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		System.out.println("Student's roll number is "+n);
+		int age=s.nextInt();
+		System.out.println("Student's roll number is "+n+" and age is: "+age);
 	}
 }
