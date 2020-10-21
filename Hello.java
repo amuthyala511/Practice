10f00f9a6cc69dd661cfc78fc8dc62ccc9f3dcd0
Aishwarya <amuthyala511@gmail.com>
2020-10-21 17:09:06 +0530

    added Hello file

--- Hello.java
+++ Hello.java
@@ -3,9 +3,5 @@ public class Hello
 	public static void main(String[] args)
 	{
 		System.out.println("Hello all");
-<<<<<<< HEAD
-=======
-		System.out.println("welcome to git branches");
->>>>>>> abc
 	}
 }
