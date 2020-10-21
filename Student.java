bac88667c52205b4acb667552fb4e952f222204f
Aishwarya <amuthyala511@gmail.com>
2020-10-21 17:38:12 +0530

    adding student file

---
+++ Student.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Student
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Enter ROLL NUMBER: ");
+		Scanner s=new Scanner(System.in);
+		int roll_no=s.nextInt();
+		System.out.println(roll_no);
+	}
+}
